0f072babee2e892d292a8233ceea0ebf2eab513b
Dhairya Jain <81606241+dhairyajain29@users.noreply.github.com>
2022-11-27 20:02:02 +0530

    Update Rangoli Studio.py
    Added polygon part

--- Rangoli Studio.py
+++ Rangoli Studio.py
@@ -98,6 +98,17 @@ def Polygon():
     SizeDialog('polygon')
     print('Polygon')
     SetColor()
+    
+    t = turtle.Turtle()
+  
+# taking input for the no of the sides of the polygon
+n = int(input("Enter the no of the sides of the polygon : "))
+  
+# taking input for the length of the sides of the polygon
+l = int(input("Enter the length of the sides of the polygon : "))
+for _ in range(n):
+    turtle.forward(l)
+    turtle.right(360 / n)
 
 def Flower():
     SizeDialog('flower')
